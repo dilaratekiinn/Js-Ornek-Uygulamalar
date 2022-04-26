88d6712686183bf8edbff6bb26f070140227b561
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-04-26 15:48:42 +0300

    first commit

---
+++ strings-uygulama/script.js
@@ -0,0 +1,25 @@
+
+
+let url="https://app.patika.dev/paths/baslangic-seviye-frontend-web-development-patikasi";
+let kursAdi="Başlangıç Seviye Frontend Web Development Patikası";
+
+// 1- url kaç karakterlidir?
+   
+let sonuc;
+
+sonuc=url.length;
+
+
+
+
+// 2- kursAdi kaç kelimeden oluşmaktadır?
+
+// 3- url https ile mi başlıyor?
+
+// 4- kursAdi içerisinde eğitimi kelimesi var mı?
+
+// 5- url ve kursAdi değişkenlerini kullanarak string bilgiyi oluşturunuz.
+
+
+
+console.log(sonuc);
